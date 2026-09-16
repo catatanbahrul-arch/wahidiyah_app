13ad2d3699418207b3c84c3593e99b111eadd953
catatanbahrul-arch <catatanbahrul@gmail.com>
2026-09-16 20:19:46 +0700

    Initialize Firebase in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,11 @@ import 'services/notification_service.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
+  // INI SAKLAR FIREBASE-NYA!
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  
   final notificationService = NotificationService();
   await notificationService.init();
   await notificationService.scheduleDailyDanaBox();
