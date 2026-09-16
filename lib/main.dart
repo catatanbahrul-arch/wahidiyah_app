8582fb800e25c8ed6f2f433cb1fc18213eee2d9c
catatanbahrul-arch <catatanbahrul@gmail.com>
2026-09-16 20:18:29 +0700

    Add Firebase core and options imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'screens/beranda_screen.dart';
 import 'screens/kalender_screen.dart';
